efd9440096dd6b48067a7a52a23a19eefd73b968
Pai Peng <pphaoniubi@gmail.com>
2024-10-12 12:13:22 +0800

    no message

--- src/main/java/com/example/article_backend/ArticleClass/ArticleController.java
+++ src/main/java/com/example/article_backend/ArticleClass/ArticleController.java
@@ -42,6 +42,10 @@ public class ArticleController {
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("Healthy");
+    }
 
 
 }
