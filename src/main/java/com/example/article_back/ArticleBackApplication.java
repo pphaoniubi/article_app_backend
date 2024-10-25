843e183bbe30bd4e2fd6ac52941c34954440db59
DESKTOP-GILPRDB\pphao <pphaoniubi@gmail.com>
2024-10-25 05:14:40 -0400

    no message

---
+++ src/main/java/com/example/article_back/ArticleBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.article_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleBackApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/article_back/ArticleClass/ArticleController.java
@@ -0,0 +1,47 @@
+package com.example.article_back.ArticleClass;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/api/articles")
+public class ArticleController {
+
+    @Autowired
+    private ArticleService articleService;
+
+    // Get all article titles
+    @GetMapping("/titles")
+    public List<Article> getAllArticleTitles() {
+        return articleService.getAllArticles();
+    }
+
+    @PutMapping("/{id}/increment-read-count")
+    public ResponseEntity<String> incrementReadCount(@PathVariable Long id) {
+        articleService.incrementReadCount(id);
+        return ResponseEntity.ok("Read count incremented.");
+    }
+
+    @GetMapping("/{id}/read-count")
+    public ResponseEntity<Integer> getReadCount(@PathVariable Long id) {
+        int readCount = articleService.getReadCount(id);
+        return ResponseEntity.ok(readCount);
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Article> getArticleById(@PathVariable Long id) {
+        return articleService.getArticleById(id)
+                .map(article -> ResponseEntity.ok(article))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
+}
