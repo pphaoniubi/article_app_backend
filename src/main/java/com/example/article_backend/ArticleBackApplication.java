3b30b499d448b6c8082114b5c6834aec92fe967d
DESKTOP-GILPRDB\pphao <pphaoniubi@gmail.com>
2024-10-25 07:19:17 -0400

    no message

--- src/main/java/com/example/article_backend/ArticleBackApplication.java
+++ src/main/java/com/example/article_backend/ArticleBackApplication.java
@@ -1,4 +1,4 @@
-package com.example.article_back;
+package com.example.article_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
